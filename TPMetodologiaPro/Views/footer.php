32bbe6e37a2ce0fb66cdf6e2620497f7ebe45809
EzequielR <ezequielrebasa@gmail.com>
2019-10-15 19:56:12 -0300

    Comente unas imagenes para mejorar vista

--- TPMetodologiaPro/Views/footer.php
+++ TPMetodologiaPro/Views/footer.php
@@ -1,11 +1,11 @@
-<div class="wrapper row4">
+<!--<div class="wrapper row4">
   <footer id="footer" class="clear"></footer>
 </div>
 <div class="wrapper row6">
   <div id="copyright" class="clear" align="center"> 
     <p >Copyright &copy; <?php echo date('Y'); ?> - All Rights Reserved - Cinema Studio</p>
   </div>
-</div>
+</div>-->
 <!-- JAVASCRIPTS -->
 <script src="../layout/scripts/jquery.min.js"></script> 
 <script src="../layout/scripts/jquery.mobilemenu.js"></script>
