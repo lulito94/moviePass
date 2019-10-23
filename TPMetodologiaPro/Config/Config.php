8ba1063e7d017e21c719b2a62c95ac5bee660ab7
EzequielR <ezequielrebasa@gmail.com>
2019-10-23 18:21:54 -0300

    ahi va mi config

--- TPMetodologiaPro/Config/Config.php
+++ TPMetodologiaPro/Config/Config.php
@@ -2,11 +2,7 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-<<<<<<< HEAD
 define("FRONT_ROOT","/TPs/TP_Final/moviePass/TPMetodologiaPro/");
-=======
-define("FRONT_ROOT","/tpFinal/moviePass/TPMetodologiaPro/");
->>>>>>> f4c1909c8398d24003814a9ba3fb764de4223739
 define("VIEWS_PATH", "Views/");
 define("API_KEY", "c65889a54974a405a970caef706f7005");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
