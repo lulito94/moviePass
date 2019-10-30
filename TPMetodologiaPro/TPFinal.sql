ad8de78558506c7e32aa4deeb971547346c381e5
EzequielR <ezequielrebasa@gmail.com>
2019-10-29 21:06:10 -0300

    modificacion en bd

--- TPMetodologiaPro/TPFinal.sql
+++ TPMetodologiaPro/TPFinal.sql
@@ -2,16 +2,6 @@ create database TPFinal;
 
 use TPFinal;
 
-create table Rooms(
-	id_room integer auto_increment,
-    seating integer not null,
-    room_name varchar(30),
-    idCinema integer,
-    
-    constraint pk_rooms primary key (id_room),
-    constraint unq_nameRoom unique (room_name),
-    constraint fk_room_cinema foreign key (idCinema) references Cinemas(idCinema)
-);
 create table Cinemas
 (
  idCinema integer auto_increment,
@@ -25,8 +15,8 @@ create table Cinemas
  constraint unq_cinemaName unique (cinemaName),
  constraint fk_cinemas_room foreign key (id_room) references Rooms(id_room)
 );
-select * 
-from Rooms;
+
+
 create table Users(
     idUser integer auto_increment,
     sex varchar(9),
@@ -47,7 +37,7 @@ create table Genres(
     name varchar(30),
     constraint pk_genre primary key (id_genre)
 );
-
+drop table Movies;
 create table Movies(
 	id integer not null,
     popularity int, 
@@ -65,7 +55,7 @@ create table Movies(
     constraint unq_movieTitle unique (title),
     constraint fk_movies_genre foreign key (id_genre) references Genres (id_genre)
 );
-
+drop table MoviesxGenres;
 create table MoviesxGenres(
 	id integer,
     id_genre integer,
@@ -74,15 +64,30 @@ create table MoviesxGenres(
     constraint fk_MoviesxGenre_movie foreign key (id) references Movies(id),
 	constraint fk_MoviesxGenre_genre foreign key (id_genre) references Genres(id_genre)
 );
-
+drop table Rooms;
+create table Rooms(
+	id_room integer auto_increment,
+    seating integer not null,
+    room_name varchar(30),
+    idCinema integer,
+    id_function integer,
+    
+    constraint pk_rooms primary key (id_room),
+    constraint unq_nameRoom unique (room_name),
+    constraint fk_room_cinema foreign key (idCinema) references Cinemas(idCinema),
+    constraint fk_room_function foreign key (id_function) references MovieFunctions(id_function)
+);
 
 create table MovieFunctions(
+	
 	id_function integer auto_increment,
+    idCinema integer,
     id_room integer,
     id integer, #Movie
-    function_time time,
+    function_time Datetime,
     
     constraint pk_movieFunction primary key (id_function),
+    constraint fk_movieFunction_cinema foreign key (idCinema)references Cinemas(idCinema),
     constraint fk_movieFunction_room foreign key (id_room) references Room (id_room),
     constraint fk_movieFunction_movie foreign key (id) references Movies (id)
 );
@@ -90,7 +95,6 @@ create table MovieFunctions(
 create table MoviesXCinema(
 	id integer,
     idCinema integer,
-    function_time time,
     ticket_price float not null,
     tickets_sold integer,
     
