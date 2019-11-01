245bb2a397b0ed7b061a1841b408d786be47dca5
lcanoro <lcanoro@temperies.com>
2019-11-01 11:35:02 -0300

    Logo fix

--- TPMetodologiaPro/Views/nav-bar.php
+++ TPMetodologiaPro/Views/nav-bar.php
@@ -1,9 +1,10 @@
 <div class="wrapper row1" style="background-image: url('https://www.cinemacenter.com.ar/images/header-bg.png');">
-  <header id="header" class="clear">
-    <div id="logo" class="fl_left" style="color: Red">
-      <img src="https://i.ibb.co/023MtCS/Logo-Makr-7km-GRP.png" style="max-width:200px; max-height: 200px" />
+    <div id="logo" class="fl_left" style="text-align: left; float: left">
+        <img src="https://i.ibb.co/023MtCS/Logo-Makr-7km-GRP.png" style="max-width:100px; max-height: 100px;" />
 
     </div>
+  <header id="header" class="clear">
+
 
    
   </header>
