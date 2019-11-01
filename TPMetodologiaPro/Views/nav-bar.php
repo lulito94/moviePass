f8bb7b6edda257c1a286abeb1900ce865033b23d
EzequielR <ezequielrebasa@gmail.com>
2019-10-31 23:55:25 -0300

    achicar logo

--- TPMetodologiaPro/Views/nav-bar.php
+++ TPMetodologiaPro/Views/nav-bar.php
@@ -1,15 +1,13 @@
 <div class="wrapper row1" style="background-image: url('https://www.cinemacenter.com.ar/images/header-bg.png');">
-  <header id="header" class="clear"> 
-    <div id="logo" class="fl_left" style="color: Red"> 
-    <img src="https://i.ibb.co/023MtCS/Logo-Makr-7km-GRP.png" width="100" height="100"></img>
+  <header id="header" class="clear">
+    <div id="logo" class="fl_left" style="color: Red">
+      <img src="https://i.ibb.co/023MtCS/Logo-Makr-7km-GRP.png" style="max-width:200px; max-height: 200px" />
 
-    <span>
-    </span>
-      </div>
-    
+    </div>
+
     <nav id="mainav" class="fl_right">
-    <a <span id="fecha" style="text-transform:uppercase;" >Fecha: <?php echo date("Y-m-d");?> </span> </a>
+      <a <span id="fecha" style="text-transform:uppercase;">Fecha: <?php echo date("Y-m-d"); ?> </span> </a>
     </nav>
-    
+
   </header>
 </div>
